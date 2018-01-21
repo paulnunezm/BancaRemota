6fe9f7ea195c6117b47fd0a4c6529617788243f2
Paul Nunez <paulnunezm@gmail.com>
2018-01-21 19:10:09 -0400

    removed: unused classes.

--- app/src/main/java/com/nunez/bancaremota/LoginActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.nunez.bancaremota
-
-import android.support.v7.app.AppCompatActivity
-
-class LoginActivity : AppCompatActivity() {
-}
--- app/src/main/java/com/nunez/bancaremota/framework/respository/data/MovieResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.nunez.palcine.framework.respository.data
-
-import com.squareup.moshi.Json
-
-data class MovieResponse(
-        @Json(name = "success") val successful: Boolean,
-        @Json(name = "result") val list: List<MovieRaw>
-)
