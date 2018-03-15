fbfc75914b168416b139cbe78c4095dd653a331a
Paul Nunez <paulnunezm@gmail.com>
2018-03-15 17:25:09 -0400

    added: loading view

--- app/src/main/java/com/nunez/bancaremota/screens/seller/winningNumbers/WinningNumbersFragment.kt
+++ app/src/main/java/com/nunez/bancaremota/screens/seller/winningNumbers/WinningNumbersFragment.kt
@@ -8,8 +8,11 @@ import com.nunez.bancaremota.framework.helpers.PreferencesManagerImpl
 import com.nunez.bancaremota.framework.respository.ServiceProvider
 import com.nunez.palcine.BaseActivity
 import com.nunez.palcine.BaseFragment
+import com.nunez.palcine.framework.extensions.gone
+import com.nunez.palcine.framework.extensions.show
 import com.nunez.palcine.framework.helpers.ConnectivityCheckerImpl
 import io.reactivex.android.schedulers.AndroidSchedulers
+import kotlinx.android.synthetic.main.progress.*
 import kotlinx.android.synthetic.main.winning_numbers.*
 
 class WinningNumbersFragment: BaseFragment(), WinningNumbersContract.View {
@@ -30,9 +33,11 @@ class WinningNumbersFragment: BaseFragment(), WinningNumbersContract.View {
     }
 
     override fun hideLoading() {
+        loadingView.gone()
     }
 
     override fun showLoading() {
+        loadingView.show()
     }
 
     override fun showNoConnectionError() {
