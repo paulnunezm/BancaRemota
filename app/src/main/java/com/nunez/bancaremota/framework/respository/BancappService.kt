1a75c4f237222b69f0eadc8b08f0165ed8fc87ba
Paul Nunez <paulnunezm@gmail.com>
2018-04-13 14:53:58 -0400

    edit: set correct return statement to getStats service method

--- app/src/main/java/com/nunez/bancaremota/framework/respository/BancappService.kt
+++ app/src/main/java/com/nunez/bancaremota/framework/respository/BancappService.kt
@@ -7,6 +7,7 @@ import com.nunez.bancaremota.framework.respository.data.User
 import com.nunez.bancaremota.screens.seller.sales.ticketBrief.PlayAvailabilityResponse
 import com.nunez.bancaremota.screens.seller.sales.ticketBrief.PrintResponse
 import com.nunez.bancaremota.screens.seller.sales.ticketBrief.TicketBriefInteractor
+import com.nunez.bancaremota.screens.seller.stats.StatsRespone
 import com.nunez.bancaremota.screens.seller.tickets.TicketsResponse
 import com.nunez.bancaremota.screens.seller.tickets.payTicketBottomSheet.PayResponse
 import com.nunez.bancaremota.screens.seller.winningNumbers.WinningNumbersResponse
@@ -45,5 +46,5 @@ interface BancappService {
     fun printTicket(@Field("ticket_id") id: String): Single<PrintResponse>
 
     @GET(Endpoints.STATS)
-    fun getStats()
+    fun getStats(): Single<StatsRespone>
 }
