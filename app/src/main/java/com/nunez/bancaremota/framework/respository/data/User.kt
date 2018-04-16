1d81de728e369b2756264507f092846a5da455bc
Paul Nunez <paulnunezm@gmail.com>
2018-04-16 18:53:12 -0400

    added: user setting fields to the user Class.
    These fields matches the response object of the /api/user node which give us the price values for each winning game for the user.

--- app/src/main/java/com/nunez/bancaremota/framework/respository/data/User.kt
+++ app/src/main/java/com/nunez/bancaremota/framework/respository/data/User.kt
@@ -1,10 +1,26 @@
 package com.nunez.bancaremota.framework.respository.data
 
-data class User(val name: String,
-                val email: String,
-                val status: String){
+import com.squareup.moshi.Json
+
+data class User(
+        val name: String,
+        val email: String,
+        val status: String,
+        @Json(name = "user_settings") val userSettings: UserSettings) {
+
     companion object {
         const val ENABLED = "enabled"
         const val DISABLED = "disabled"
     }
 }
+
+data class UserSettings(
+        @Json(name = "q_first") val quinielaFirst: String,
+        @Json(name = "q_second") val quinieelaSecond: String,
+        @Json(name = "q_third") val quinielaThird: String,
+        @Json(name = "p_first") val paleFirst: String,
+        @Json(name = "p_second") val paleSecond: String,
+        @Json(name = "super_p") val superPale: String,
+        @Json(name = "t_first") val tripletaFirst: String,
+        @Json(name = "t_second") val tripletaSecond: String
+)
