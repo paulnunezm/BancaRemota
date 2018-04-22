78b9736f88a259dbf6af8ad72677a0ac4264c58b
Paul Nunez <paulnunezm@gmail.com>
2018-04-22 16:35:58 -0400

    added: show seller name on drawer

--- app/src/main/java/com/nunez/bancaremota/screens/seller/SellerActivity.kt
+++ app/src/main/java/com/nunez/bancaremota/screens/seller/SellerActivity.kt
@@ -1,12 +1,16 @@
 package com.nunez.bancaremota.screens.seller
 
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.view.View
+import android.widget.TextView
 import com.nunez.bancaremota.R
 import com.nunez.bancaremota.framework.helpers.PreferencesManagerImpl
 import com.nunez.palcine.BaseActivity
 import kotlinx.android.synthetic.main.activity_seller.*
 
-class SellerActivity: BaseActivity() {
+
+class SellerActivity : BaseActivity() {
     override var toolbarId: Int? = R.id.toolbar
     override var layout: Int = R.layout.activity_seller
     override var drawerId: Int? = R.id.drawer_layout
@@ -23,5 +27,15 @@ class SellerActivity: BaseActivity() {
                 drawerManager)
 
         drawerNavigator.navigateTo(R.id.sale)
+        setSellerNameOnDrawer()
+    }
+
+    private fun setSellerNameOnDrawer() {
+        val preferencesManager = PreferencesManagerImpl(this)
+        val user = preferencesManager.retrieveUserInfo()
+        val navigationView = findViewById<View>(R.id.nav_view) as NavigationView
+        val headerView = navigationView.getHeaderView(0)
+        val sellerName = headerView.findViewById(R.id.sellerName) as TextView
+        sellerName.text = user.name
     }
 }
